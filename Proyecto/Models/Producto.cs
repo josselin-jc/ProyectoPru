34043fe1800056d46750c69b0762afb9c6b3dc37
Joss_JC <jossj60@gmail.com>
2021-12-01 17:17:51 -0600

    01/12

---
+++ Proyecto/Models/Producto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto.Models
+{
+    public class Producto
+    {
+        //Objetos que va a tener el producto
+
+        public int IdProducto { get; set; }
+        public string Nombre { get; set;}
+        public decimal Precio { get; set; }
+        public string RutaImagen { get; set; }
+    }
+}
