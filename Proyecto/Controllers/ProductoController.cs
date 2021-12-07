000b699d60aacb14e048bd06699e40e0e958fb6d
Joss_JC <jossj60@gmail.com>
2021-12-06 18:08:16 -0600

    06/12

---
+++ Proyecto/Controllers/ProductoController.cs
@@ -0,0 +1,69 @@
+﻿using Proyecto.Models;
+using Proyecto.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto.Controllers
+{
+    public class ProductoController : Controller
+    {
+        RepositoryProducto repo;
+        public ProductoController()
+        {
+            repo = new RepositoryProducto();
+        }
+
+        //Nos va a permitir mostrar los productos
+        public ActionResult ListaProductos(int? id)
+        {
+            if (id != null)
+            {
+                List<int> codigosProductos;
+                if (Session["PRODUCTOS"] == null)
+                {
+                    codigosProductos = new List<int>();
+                }
+                else
+                {
+                    codigosProductos = Session["PRODUCTOS"] as List<int>;
+                }
+                codigosProductos.Add(id.Value);
+                Session["PRODUCTOS"] = codigosProductos;
+                ViewBag.Mensaje = "Productos en Carrito: " + codigosProductos.Count();
+            }
+            ViewBag.Carrito = Session["PRODUCTOS"];
+            List<Producto> prod = this.repo.GetProductos();
+            return View(prod);
+        }
+        //nos permite ver los productos que tenemos en el carrito
+        public ActionResult ProductosCarrito(int? id)
+        {
+            if (id != null)
+            {
+                List<int> lista = (List<int>)Session["PRODUCTOS"];
+                lista.Remove(id.GetValueOrDefault());
+                if (lista.Count() == 0)
+                {
+                    Session["PRODUCTOS"] = null;
+                }
+                else
+                {
+                    Session["PRODUCTOS"] = lista;
+                }
+            }
+            if (Session["PRODUCTOS"] == null)
+            {
+                return View();
+            }
+            else
+            {
+                List<int> lista = (List<int>)Session["PRODUCTOS"];
+                List<Producto> prod = this.repo.BuscarProductos(lista);
+                return View(prod);
+            }
+        }
+    }
+}
--- Proyecto/Repositories/RepositoryProducto.cs
+++ Proyecto/Repositories/RepositoryProducto.cs
@@ -7,7 +7,26 @@ using System.Web;
 
 namespace Proyecto.Repositories
 {
-
-    public class RepositoryProducto { }
+    public class RepositoryProducto
+    {
+        ContextoProducto contexto;
+        public RepositoryProducto()
+        {
+            this.contexto = new ContextoProducto();
+        }
+        //Creamos metodos para obtener una lista de productos 
+        //Primero obtenemos los datos 
+        public List<Producto> GetProductos()
+        {
+            var consulta = from datos in contexto.Productos select datos;
+            return consulta.ToList();
+        }
+        //Segundo obtenemos los id donde se guardan los productos donde  la consulta busca el espefifico id
+        public List<Producto> BuscarProductos(List<int> id)
+        {
+            var consulta = from datos in contexto.Productos where id.Contains(datos.IdProducto) select datos;
+            return consulta.ToList();
+        }
+    }
    
 }
